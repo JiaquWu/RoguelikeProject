915fcabe4cc7181fc9d4828e935830c07fc5194f
JiaquWu <wujiaqu1233@gmail.com>
2022-12-02 14:56:43 +0100

    delete all

--- Source/RoguelikeProject/RoguelikeProject.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "RoguelikeProject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RoguelikeProject, "RoguelikeProject" );
